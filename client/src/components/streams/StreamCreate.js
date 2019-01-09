75fd2b1e908877c10bf6be096e9ff04822bdec4c
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-09 13:54:33 +0900

    add label name

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 class StreamCreate extends React.Component {
-  renderInput(formProps) {
+
+  // input is injected by redux-form
+  renderInput({ input, label }) {
     //console.log(formProps)
     // <input onChange={formProps.input.onChange} value={formProps.input.value} />
     return (
-      <input {...formProps.input} />
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
     )
   }
 
   render() {
     console.log(this.props)
     return (
-      <form>
-        <Field name="title" component={this.renderInput}/>
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title"/>
+        <Field name="description" component={this.renderInput} label="Enter Description" />
       </form>
     )
   }
