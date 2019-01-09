0449deb4117aeb235710c43b704572c7401f4ca1
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-09 18:02:54 +0900

    add stream list render

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,42 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStreams } from '../../actions'
 
-const StreamList = () => {
-  return <div>StreamList</div>
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams()
+  }
+
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          {stream.title}
+          <div className="description">{stream.description}</div>
+        </div>
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default StreamList
+const mapStateToProps = (state) => {
+  // convet object to array
+  // if you deal with records...
+  // in component use array
+  // in store(for update delte etc) use object
+  return { streams: Object.values(state.streams) }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList)
