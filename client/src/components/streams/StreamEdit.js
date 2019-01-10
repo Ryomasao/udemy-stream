d78201df2f03f3367d2de06caa922350768fa55f
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-10 11:50:22 +0900

    不要なパラメータをformに渡さないようにpick関数を使う

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import React from 'react'
 import { connect } from 'react-redux'
 import { fetchStream, editStream } from '../../actions'
@@ -21,10 +22,15 @@ class StreamEdit extends React.Component {
       return <div></div>
     }
 
+    // initialValueを渡すときに、変更する必要のない、id,userIdがformに渡り、そのままリクエストに繋がる　
+    // この段階で、不要な値は渡さないように_pick関数を使う。
     return (
       <div>
         <h3>Edit a Stream</h3>
-        <StreamForm  onSubmit={this.onSubmit} initialValues={this.props.stream}/>
+        <StreamForm  
+          onSubmit={this.onSubmit} 
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+        />
       </div>
     )
   }
