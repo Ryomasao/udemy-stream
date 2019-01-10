b0325a9ebc02a3b0fc0cd501c073290e9252af3b
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-10 13:44:26 +0900

    add modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import history from '../history'
 
 const Modal = props => {
+  // e.stopPropagation 
+  // 子要素のモーダルをクリックしたときに、親要素(dimmer)のクリックイベントが発生しないようする。
+  // eventのバブルアップを止める
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        test
+    <div 
+      onClick={() => history.push('/') } 
+      className="ui dimmer modals visible active"
+     >
+      <div 
+        onClick={(e) => e.stopPropagation ()} 
+        className="ui standard modal visible active"
+       >
+        <div className="header"> Delete Stream</div>
+        <div className="content"> 
+          Are you sure?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.querySelector('#modal')
