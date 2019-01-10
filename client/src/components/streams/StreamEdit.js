f6512b03841e129a04e84a1dfc590d5ba61adcd6
ryoji.yamauchi <ryoji_yamauchi@waku-2.com>
2019-01-10 11:02:37 +0900

    add fetch on EditList

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>
+// データはReduxからとってくるんじゃなくって、都度APIを叩いた方がいい
+
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  render() {
+    // https://reactjs.org/docs/react-component.html
+    // render() -> componentDidMount()
+    if(!this.props.stream) {
+      return <div></div>
+    }
+
+    return  <div>{this.props.stream.title}</div>
+  }
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit)
